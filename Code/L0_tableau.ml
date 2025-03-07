fa60eb6b6829d9d08ee1895e8f37bf3d73849300
SkyTryX <dorian.gil34@gmail.com>
2025-03-07 14:33:01 +0100

    updates

--- Code/L0_tableau.ml
+++ Code/L0_tableau.ml
@@ -1,10 +1,25 @@
-type symbol = 
-  Const of bool | 
-  And of symbol * symbol | 
-  Or of symbol * symbol |
-  Not of symbol |
-  Implies of symbol * symbol |
-  Equivalent of symbol * symbol;;
+type boolean = True | False
 
-type 'a tree = Nil | Node of 'a tree * 'a * 'a tree;;
+type formula = 
+  Literal of boolean | 
+  And of formula * formula | 
+  Or of formula * formula |
+  Not of formula |
+  Implies of formula * formula |
+  Equivalent of formula * formula;;
 
+type 'a tree = Nil | Node of 'a tree * 'a array * 'a tree;;
+
+(* Fonction qui a une formule avec des hypothèses associe son arbre de décision *)
+let formula2tree (f: formula) (hyp: formula array) : formula tree = match f with
+  | Literal(_) -> Nil  
+  | Not(Literal(_)) -> Nil
+  | Or(a, b) -> Node(formula2tree a hyp, hyp, formula2tree b hyp)
+  | And(a,b) -> Nil
+  | Implies(a,b) -> Node(formula2tree Not(a) hyp, hyp, formula2tree b hyp)
+  | Equivalent(a,b) -> Nil
+  | Not(Not(a)) -> Node(formula2tree a hyp, hyp, Nil)
+  | Not(Or(a, b)) -> Nil
+  | Not(And(a,b)) -> Node(formula2tree Not(a) hyp, hyp, formula2tree Not(b) hyp)
+  | Not(Implies(a,b)) -> Nil
+  | Not(Equivalent(a,b)) -> Nil
