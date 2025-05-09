3c1e2ab9d83ed24fb14fb24bb9f84a3e6a59497d
SkyTryX <dorian.gil34@gmail.com>
2025-05-09 14:09:54 +0200

    updates

--- L0_tableau.ml
+++ L0_tableau.ml
@@ -14,6 +14,18 @@ let expand formula =
   | Or (f1, f2) -> [[f1]; [f2]]
   | _ -> [];;
 
+let rec print_formula (f:formula) = match f with
+  | Atom(s) -> print_string s
+  | Not f -> print_string "Not("; print_formula f;print_string ")"
+  | And (f,g) -> print_formula f;print_string " And ";print_formula g
+  | Or (f,g) -> print_formula f;print_string " Or ";print_formula g;;
+
+let rec print_branches (b:formula list list) =
+  print_string "[";
+  List.iter (fun y -> print_string "[";List.iter (fun x -> print_formula x; print_string ", ";) y;print_string "]";) b;
+  print_string "]";;
+
+
 (* On regarde si il existe un cycle i.e une contradiction *)
 let has_cycle branch =
   List.exists (fun f -> List.mem (Not f) branch) branch;;
@@ -25,21 +37,20 @@ let rec tableau branches =
 
   if has_cycle branch then (* Si la branche selectionné a un cycle *)
     tableau rest (* On vérifie le reste *)
-  else (* Sinon, on le developpe *)
+  else 
+    (* Sinon, on la developpe *)
     match branch with
     | [] -> true (* Une branche non fermé pas developpable, c'est gagné! *)
     | f :: fs ->
-
+    
     let expansions = expand f in match expansions with
     | [] -> tableau (fs :: rest) (* rien n'a developpé, on continue *)
     | new_branches ->
       
     let expanded_branches = List.map (fun b -> b @ fs) new_branches in
     tableau (expanded_branches @ rest);; (* On appelle recursivement sur les versions developpés des branches*)
+   
 
-(* Petite fonction *)
 let is_satisfiable formula =
   let initial_branch = [formula] in tableau [initial_branch];;
-  
-let f = And (And(Atom "P", Not(Atom "P")), Atom "Q") in
-is_satisfiable f;;
+
